0375357ec3bd923766d91d06187870c42518d565
Stephen Paul <iamstepaul@gmail.com>
2023-04-01 20:15:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# OpenClassrooms-Student-Center fullstack-activity #
+
+## Installation ##
+Clone this repo, and run `npm install` from within the project directory.
+
+## Using the app ##
+Run `npm start` from within the project directory.
+
+## Schema ##
+```
+mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  inStock: { type: Boolean, required: true }
+})
+```
+## Available API endpoints ##
+The frontend app requires the following endpoints with the correct behavior for all tests to pass:
+* GET `/api/products`
+
+Returns all products as `{ products: Product[] }`
+* GET: `/api/products/:id` 
+
+Returns product for given ID as `{ product: Product }`
+* POST: `/api/products`
+Creates product in database.
+
+Returns product created in database (including `_id` field) as `{ product: Product }`
+* PUT: `/api/products/:id`
+
+
+Returns `{ message: 'Modified!' }`
+* DELETE: `/api/products/:id`
+
+Deletes product with given ID.
+
+Returns `{ message: 'Deleted!' }`
---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const Product = require('./models/products')
+
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://stephen:jQPGOlgJi9ifDCz2@cluster0.kjtenpj.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    console.log('successfully connected to mongodb atlas')
+})
+.catch((error) => {
+    console.log("Unable to connect to mongodb atlas")
+})
+
+
+// cors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.get('/api/products', async (req, res) => {
+    const products = await Product.find();
+    res.json({ products });
+  });
+  
+  app.get('/api/products/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.json({ product });
+  });
+  
+  app.post('/api/products', async (req, res) => {
+    const { name, description, price, inStock } = req.body;
+    const product = new Product({ name, description, price, inStock });
+    await product.save();
+    res.json({ product });
+  });
+  
+  app.put('/api/products/:id', async (req, res) => {
+    const { name, description, price, inStock } = req.body;
+    await Product.findByIdAndUpdate(req.params.id, { name, description, price, inStock });
+    res.json({ message: 'Modified!' });
+  });
+  
+  app.delete('/api/products/:id', async (req, res) => {
+    await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Deleted!' });
+  });
+
+module.exports = app;
